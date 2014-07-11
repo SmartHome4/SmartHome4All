e4493e6b4db49f927f2422eb0c5f866c56f151ba
Oleg Fominsky <oleg.fominsky@mail.ru>
2014-07-11 14:04:05 +0400

    Класс для работы с базой данных умного дома. Реализована только функция подключения к базе.

---
+++ SmartDB.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of SmartDB
+ *
+ * @author Олег
+ */
+class SmartDB {
+    
+    public 
+            function connect ($name, $user, $pass) {
+        $server = $name;
+        $db_pass = $pass;
+        $db_user = $user;
+        mysql_connect($server, $db_user, $db_pass);
+        if(mysql_query("use SmartHouse")){
+        echo "Успех";}
+        else{
+        echo "Неудача";}
+        }
+        
+    }
+            
+            
+        
+                
+
